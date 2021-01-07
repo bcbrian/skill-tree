3ad9e013e92fc762ec8947e0664fd19a1e85aede
Brian Bartholomre <me@bcbrian.com>
2021-01-07 11:18:15 -0700

    some failing tests to start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,54 +1,75 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 // write some basic test to know that the criteria is met
 
 test('Left click to add points.', () => {
+  // given all skill points are not used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // select a unslected skill
+  const leftClick = { button: 0 }
+  fireEvent.click(screen.queryByAltText(/skill/i), leftClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
 
 test('Right click to remove points.', () => {
+  // given all skill points are used not used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // unselect a selected skill
+  const rightClick = { button: 2 }
+  fireEvent.click(screen.queryByAltText(/skill/i), rightClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
 
 test('The user may only use up to 6 points.', () => {
+  // given all skill points are used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // select a unslected skill
+  const rightClick = { button: 0 }
+  fireEvent.click(screen.queryByAltText(/skill/i), rightClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
 
 test('Each item only accounts for one point.', () => {
+\
+  // same as a differnt test just checking a different outcome
+
+  // given all skill points are not used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // select a unslected skill
+  const leftClick = { button: 0 }
+  fireEvent.click(screen.queryByAltText(/skill/i), leftClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
 
 test('Displays current point total', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
+  const totalPoints = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
   expect(false).toBeTruthy();
 });
 
-test('The user must select the items in order.', () => {
+test('The user must select the items in order. Select the next one.', () => {
+  // given all skill points are not used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // select a unslected skill
+  const leftClick = { button: 0 }
+  fireEvent.click(screen.queryByAltText(/skill/i), leftClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
 
-test('For example: The user may not put a point in the cake without first having put points in the chevrons and the silverware (in that order).', () => {
+test('The user must select the items in order. Try to skip one.', () => {
+  // given all skill points are not used
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  // select a unslected skill
+  const leftClick = { button: 0 }
+  fireEvent.click(screen.queryByAltText(/skill/i), leftClick);
+  // update something and check that it is updated...
   expect(false).toBeTruthy();
 });
